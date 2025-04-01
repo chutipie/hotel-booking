dd1fe6d9cf4c3a687e1ca1f75ab846a533dc838a
Matthew Corral <matthew_corral@dlsu.edu.ph>
2025-04-02 04:14:53 +0800

    Fix port reference in server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
 // Middleware
 app.use(express.json());
