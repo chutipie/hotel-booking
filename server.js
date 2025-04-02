58e483bbf6235661611336f804beb7866ba2db15
Matthew Corral <matthew_corral@dlsu.edu.ph>
2025-04-02 11:24:34 +0800

    Fixed PORT issue and removed deprecated MongoDB options

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
